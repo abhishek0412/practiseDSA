e22aa9cc32239145abdbfaa45a5a2d7e8c1507b6
abhishek choudhary <abhishekr.choudhar@nuance.com>
2024-01-03 20:44:16 +0530

    added the test to find the largest number in Integer array

---
+++ practiseDSA/arrayOP.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiseDSA
+{
+    public class arrayOP
+    {
+
+        public int findLargetsInArray(int[] problem)
+        {
+            if (problem == null || problem.Length == 0)
+            {
+                throw new ArgumentException("Array is null or empty.");
+            }
+            int solution = problem[0];
+
+            foreach(int i in problem)
+            {
+                if (i > solution)
+                {
+                    solution = i;
+                }
+            }
+
+            return solution;
+        }
+    }
+}
---
+++ practiseDSATests/arrayOPTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using practiseDSA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiseDSA.Tests
+{
+    [TestClass()]
+    public class arrayOPTests
+    {
+        [TestMethod()]
+        public void findLargetsInArrayTest()
+        {
+            arrayOP aOP = new arrayOP();
+            Assert.AreEqual(99, aOP.findLargetsInArray( new int[] { 2, 8, 2, 1, 4, 9, 4, 3, 66, 99, 33, 23 }));
+        }
+    }
+}
