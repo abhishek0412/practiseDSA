85526c09319940d8434d98090de2d64dceaf8d4c
abhishek choudhary <abhishekr.choudhar@nuance.com>
2024-01-03 19:12:11 +0530

    Add project files.

---
+++ practiseDSA/stringsOP.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiseDSA
+{
+    public class stringsOP
+    {
+        //In C#, string is keyword which is an alias for System.String class.
+        //That is why string and String are equivalent.
+        //We are free to use any naming convention.
+        string testName = "Test"; //creating string using string keyword
+        String testDescsrption = "Test Description";//creating string using String class  
+
+        static char[] ch = { 'c', 's', 'h', 'a', 'r', 'p' };
+        string testTest = new string(ch);
+
+        public string reverseString(string problem)
+        {
+            string solution = string.Empty;
+
+            return solution;
+        }
+    }
+}
---
+++ practiseDSATests/stringsOPTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using practiseDSA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiseDSA.Tests
+{
+    [TestClass()]
+    public class stringsOPTests
+    {
+        [TestMethod()]
+        public void reverseString()
+        {
+            Assert.Fail();
+        }
+    }
+}
